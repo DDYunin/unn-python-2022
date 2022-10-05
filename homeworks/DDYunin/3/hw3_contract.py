0193dae8c4bfbc9834e4130b7e93c085da644cf1
Dan <Daniilunin@yandex.ru>
2022-10-05 10:21:32 +0300

    Update hw3

--- homeworks/DDYunin/3/hw3_contract.py
+++ homeworks/DDYunin/3/hw3_contract.py
@@ -1,56 +1,66 @@
 class ContractError(Exception):
     """We use this error when someone breaks our contract."""
+    # Для того, чтобы выдать исключения требуются: __init__ и __str__
+    def __init__(self, *args):
+        if args is None:
+            self.message = "Стандартный текст ошибки контракта!"
+        else:
+            self.message = args[0]
 
+    def __str__(self):
+        return f"Произошла ошибка с текстом: {self.message}"
 
 #: Special value, that indicates that validation for this type is not required.
 Any = object()
 
 
 def contract(arg_types=None, return_type=None, raises=None):
-    def mydecorator(mfunction):
+    def my_decorator(my_function):
         def wrapped(*args):
-            # вызов функции
-            if return_type is not None:
-                if return_type != type(mfunction(*args)):
-                    raise ContractError("Error! Uncorrect return type")
-            if arg_types != None:
+            # Сначала првоеряем на соответствие
+            if arg_types is None:
+                pass
+            else:
                 for arg, arg_type in zip(args, arg_types):
                     if type(arg) != arg_type and arg_type != Any:
-                        raise ContractError("Error! Uncorrect arg_types")
+                        raise ContractError("Error! Contract fail! Uncorrect arg_types") from TypeError
+            # Проверяем возвращаемый тип на соответствие
+            if return_type is None:
+                pass
+            else:
+                if return_type != type(my_function(*args)) and return_type != Any:
+                    raise ContractError("Error! Contract fail! Uncorrect return type") 
             try:
-                mfunction(*args)
+                # вызов функции
+                result = my_function(*args)
             except:
                 raise raises
-            return mfunction(*args)
+            return result
         return wrapped
-    return mydecorator
+    return my_decorator
 
 if __name__ == '__main__':
+    
+    @contract(arg_types=(int, int), return_type=Any)
+    def add_two_numbers(first, second):
+        return first + second
+
+    add_two_numbers(1, 2)  # ok
+    # add_two_numbers('a', 'b')  # raises ContractError
+
     @contract(arg_types=(int, int), return_type=float, raises=(ZeroDivisionError,))
     def div(first, second):
         return first / second
 
-    print(div(1, 2))  # ok
-    # print(div(1, 0))  # raises ZeroDisionError
-    # print(div(1, None))  # raises ContractError from TypeError
+    div(1, 2)  # ok
+    # div(1, 0)  # raises ZeroDisionError
+    # div(1, None)  # raises ContractError from TypeError
 
-
-    @contract(arg_types=(int, int), return_type=int)
-    def add_two_numbers(first, second):
-        return first + second
-
-    print(add_two_numbers(1, 2))  # ok
-
-    @contract(arg_types=(str, str))
-    def add_str(first, second):
-        return first + second
-
-    print(add_str("Hello", "World"))
-
+    # validates only return type, args and raises are ignored:
     @contract(arg_types=(int, Any))
-    def add_two_numbers2(first, second):
-        return first + second
+    def subtract_two_numbers(first, second):
+        return first - second
 
-    print(add_two_numbers2(1, 2))  # ok
-    print(add_two_numbers2(1, 3.4))  # ok
-    # print(add_two_numbers2(2.1, 1))  # raises ContractError
+    subtract_two_numbers(1, 2)  # ok
+    subtract_two_numbers(1, 3.4)  # ok
+    # subtract_two_numbers(2.1, 1)  # raises ContractError
