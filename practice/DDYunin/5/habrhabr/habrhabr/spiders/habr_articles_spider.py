ce821473ff9e094ada546a1c7a4d85174ad157e1
Dan Yunin <DaniilUnin2002@yandex.ru>
2022-10-26 22:47:14 +0300

    Make parser, that take titles all posts of user

--- practice/DDYunin/5/habrhabr/habrhabr/spiders/habr_articles_spider.py
+++ practice/DDYunin/5/habrhabr/habrhabr/spiders/habr_articles_spider.py
@@ -1,9 +1,29 @@
+from subprocess import call
 import scrapy
 
 class ArticleSpider(scrapy.Spider):
     name = 'habr'
     start_urls = ['https://habr.com/ru/users/reug/posts']
 
+# Первоначальная ссылка, которая ведёт в посты пользователяку
+# response.css('span.tm-tabs__tab-item a::attr(href)')[1].get()
+
+# Количество постов
+# response.css('span.tm-tabs__tab-counter::text').get().strip()
+
+# Ссылка на первый пост и соответсвенно и так далее
+# response.css('a.tm-article-snippet__readmore::attr(href)')[0].get()
+
+# Название статьи
+# response.css('h1.tm-article-snippet__title span::text').get()
+
     def parse(self, response):
-        for link in response.css('dev.tm-article-body a::attr(href)'):
-            print(link)
+        link_user_posts = response.css('span.tm-tabs__tab-item a::attr(href)')[1].get()
+        yield response.follow(link_user_posts)
+        for link in response.css('a.tm-article-snippet__readmore::attr(href)'):
+            yield response.follow(link, callback=self.parse_post)
+
+    def parse_post(self, response):
+        yield {
+            'title':str(response.css('h1.tm-article-snippet__title span::text').get())
+        }
